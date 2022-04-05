0519a384cfdc2ee44cad086a129a071d10a5148e
alireza <alirezahekmati80@gmail.com>
2022-04-05 07:23:58 -0700

    jh

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app =Vue.createApp({
+    data(){
+        return{
+            product:'Socks',
+            description:'i am learning vue beacause the job want me to solve a problem with vue'
+        }
+    }
+})
